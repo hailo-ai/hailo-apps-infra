fd483857d15b080a0c9cae9adab1c5e62522a0f0
michaelf <michaelf@hailo.ai>
2025-03-02 15:17:03 +0200

    detection yolo v11

--- download_resources.sh
+++ download_resources.sh
@@ -14,14 +14,21 @@ download_model() {
     file_name="scdepthv3_h8l.hef"
   fi
 
-  if [ ! -f "$file_name" ]; then
+  # Check if the file is yolov11m.hef and part of H8L_HEFS
+  if [[ "$url" == *"yolov11s.hef" && "$url" == *"hailo8l"* ]]; then
+    file_name="yolov11s_h8l.hef"
+  fi
+
+  local file_path="$RESOURCE_DIR/$file_name"
+
+  if [ ! -f "$file_path" ]; then
     echo "Downloading $file_name..."
-    wget -q --show-progress "$url" -O "$file_name" || {
+    wget -q --show-progress "$url" -O "$file_path" || {
       echo "Failed to download $file_name after multiple attempts."
       exit 1
     }
   else
-    echo "File $file_name already exists. Skipping download."
+    echo "File $file_name already exists in $RESOURCE_DIR. Skipping download."
   fi
 }
 
@@ -31,6 +38,7 @@ H8_HEFS=(
   "https://hailo-model-zoo.s3.eu-west-2.amazonaws.com/ModelZoo/Compiled/v2.13.0/hailo8/yolov5m_seg.hef"
   "https://hailo-model-zoo.s3.eu-west-2.amazonaws.com/ModelZoo/Compiled/v2.13.0/hailo8/yolov8m.hef"
   "https://hailo-model-zoo.s3.eu-west-2.amazonaws.com/ModelZoo/Compiled/v2.14.0/hailo8/scdepthv3.hef"
+  "https://hailo-model-zoo.s3.eu-west-2.amazonaws.com/ModelZoo/Compiled/v2.14.0/hailo8/yolov11m.hef"
 )
 
 H8L_HEFS=(
@@ -38,6 +46,7 @@ H8L_HEFS=(
   "https://hailo-csdata.s3.eu-west-2.amazonaws.com/resources/hefs/h8l_rpi/yolov5n_seg_h8l_mz.hef"
   "https://hailo-csdata.s3.eu-west-2.amazonaws.com/resources/hefs/h8l_rpi/yolov8s_pose_h8l.hef"
   "https://hailo-model-zoo.s3.eu-west-2.amazonaws.com/ModelZoo/Compiled/v2.14.0/hailo8l/scdepthv3.hef"
+  "https://hailo-model-zoo.s3.eu-west-2.amazonaws.com/ModelZoo/Compiled/v2.14.0/hailo8l/yolov11s.hef"
 )
 
 VIDEOS=(
